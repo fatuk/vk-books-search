0053df66f5337f526459afb7883441ce2bbf27c7
fatuk <fatukk@gmail.com>
2015-01-02 02:01:12 +0300

    Add public custom search

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(function () {
-	var url = 'https://www.googleapis.com/customsearch/v1?';
+	/*var url = 'https://www.googleapis.com/customsearch/v1?';
 	jQuery.getJSON(url, {
 		key: 'AIzaSyDN-qbTTNWQOpapAax85GQvtIU4ZbzLHeA',
 		cx: '000420937498810841983:v9zyg2gy-g0',
 		q: 'site:vk.com+мир+кольцо+mobi+inurl:?hash=&oq=site:vk.com+мир+кольцо+mobi+inurl:?hash='
 	}, function (json, textStatus) {
 		console.log(json, textStatus);
-	});
+	});*/
 
 });
