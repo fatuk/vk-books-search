c00ac844bd2e36fef15db159de242bf42602a0ea
fatuk <fatukk@gmail.com>
2015-01-02 23:52:51 +0300

    Fix app.js

--- js/app.js
+++ js/app.js
@@ -4,29 +4,41 @@ $(function () {
 		jQuery.getJSON(url, {
 			key: 'AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY',
 			rsz: 'filtered_cse',
-			num: '10',
+			num: '6',
 			hl: 'ru',
 			prettyPrint: 'false',
 			source: 'gcsc',
 			gss: '.io',
 			sig: '23952f7483f1bca4119a89c020d13def',
 			cx: '000420937498810841983:v9zyg2gy-g0',
-			q: 'site:vk.com м' + query + ' ' + format + ' inurl:?hash='
+			q: 'site:vk.com ' + query + ' ' + format + ' inurl:?hash='
 		}, function (json, textStatus) {
 			var $container = $('.js-container');
-			var results = _.map(json.results, function (item) {
-				item.url = decodeURIComponent(item.url);
-				return item;
-			});
-			json.results = results;
+			// console.log(json);
 			var rendered = Mustache.render($('#searchItemTemplate').html(), json);
 			$container.html(rendered);
+		});
+	};
+
+	var getBookInfo = function (query) {
+		var url = 'https://www.googleapis.com/books/v1/volumes?';
+		jQuery.getJSON(url, {
+			q: query
+		}, function (json, textStatus) {
+			// console.log(json.items[0].volumeInfo.title);
+			_.each(json.items, function (item, k) {
+				if (k <= 10) {
+					console.log(item.volumeInfo.title);
+				}
+				// console.log(item.volumeInfo.title);
+			});
 		});
 	};
 
 	var options = {
 		callback: function (value) {
 			var format = $('.js-searchSelect').val();
+			// getBookInfo(value);
 			sendQuery(value, format);
 		},
 		wait: 300,
